df7af36136e5559ff72f94c37735ae72126cdef4
igorbaesse86@gmail.com <igorbaesse86@gmail.com>
2018-07-13 23:31:08 -0300

    create server response

--- Aula 1/hello_world.js
+++ Aula 1/hello_world.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    var categoria = req.url
+    switch (categoria) {
+        case '/moda':
+            res.end("<html><body>Portal de moda</body></html>")
+            break;
+        case '/tecnologia':
+            res.end("<html><body>Portal de tecnologia</body></html>")
+            break;
+        default:
+            res.end("<html><body>Portal de default</body></html>")
+
+    }
+});
+server.listen(3000);
